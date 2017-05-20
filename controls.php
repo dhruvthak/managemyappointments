efa2bd83a74cfbd35bcea847aa9df4f65232740a
Dhruv <dhruvthak@gmail.com>
2017-05-20 19:36:58 -0400

    Adding back-end controls to the app. and minor setup changes.

--- controls.php
+++ controls.php
@@ -1,3 +1,27 @@
 <?php
 
+   $dbhost = 'localhost';
+   $dbuser = 'root';
+   $dbpass = 'june1692';
+   $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+   $result = array();
+
+   if(! $conn ) {
+      die('Could not connect: ' . mysqli_error());
+   }
+   
+   $sql = 'SELECT * FROM tb_appointments';
+   mysqli_select_db($conn,'appointments_db');
+   $retval = mysqli_query($conn,$sql );
+   
+   if(! $retval ) {
+      die('Could not get data: ' . mysqli_error($conn));
+   }
+   
+   while($row = mysqli_fetch_assoc($retval)) {
+      $result[] = $row;
+   }
+   
+   print json_encode($result);
+   mysqli_close($conn);
 ?>
